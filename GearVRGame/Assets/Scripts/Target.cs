49811ee81fb4d54ae665d21871648e98abcc9ef8
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2018-12-18 00:59:28 +0100

    Gun added

---
+++ GearVRGame/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float health = 5f;
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health < 1)
+        {
+            Die();
+        }
+    }
+    
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
