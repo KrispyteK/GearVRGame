ce8649dbac23573ca6ea315afa6767089d880e2d
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2019-01-09 18:03:49 +0100

    Error

--- GearVRGame/Assets/Scripts/Progress.cs
+++ GearVRGame/Assets/Scripts/Progress.cs
@@ -6,34 +6,11 @@ using UnityEngine.UI;
 public class Progress : MonoBehaviour {
 
     public GameManager GameManager;
-    public Slider progressBar;
-
-    public static bool TVOn = true;
-    public static bool TVStage1 = false;
-    public static bool TVStage2 = true;
-
-    private void Start()
-    {
-        InvokeRepeating("EnergyCheck", 1.0f, 1.0f);
-    }
+    static Slider progressBar;
 
     // Update is called once per frame
     void Update () {
         progressBar.value = (GameManager.TargetEnergy - GameManager.EnergyWastage) / GameManager.TargetEnergy * 100;
 	}
 
-    void EnergyCheck()
-    {
-        if (TVOn)
-        {
-            if (TVStage1)
-            {
-                progressBar.value -= 1;
-            }
-            if (TVStage2)
-            {
-                progressBar.value -= 2;
-            }
-        }
-    }
 }
