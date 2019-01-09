e929bb24572ed1d6770d552b12b81697f93e41f0
GSjoerdstra <45204487+GSjoerdstra@users.noreply.github.com>
2019-01-09 16:27:40 +0100

    Energy goes down
    Energy goes down based on if a "TVOn" is true or not. Will also check which stage the TV is.

--- GearVRGame/Assets/Scripts/Progress.cs
+++ GearVRGame/Assets/Scripts/Progress.cs
@@ -8,9 +8,32 @@ public class Progress : MonoBehaviour {
     public static int progress = 100;
     public Slider progressBar;
 
-	// Update is called once per frame
-	void Update () {
+    public static bool TVOn = true;
+    public static bool TVStage1 = false;
+    public static bool TVStage2 = true;
+
+    private void Start()
+    {
+        InvokeRepeating("EnergyCheck", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
         progressBar.value = progress;
-        print("progress " + progress);
 	}
+
+    void EnergyCheck()
+    {
+        if (TVOn)
+        {
+            if (TVStage1)
+            {
+                progress -= 1;
+            }
+            if (TVStage2)
+            {
+                progress -= 2;
+            }
+        }
+    }
 }
