3420083fc03946da74a21ae6180ad611c697c5c6
Torresus5 <mazylis.tomas@gmail.com>
2019-01-12 00:15:32 +0100

    Progress Halo bar(texture is still buggy)

---
+++ GearVRGame/Assets/ScoreUfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUfo : MonoBehaviour {
+
+    public Renderer render;
+    GameManager GameManager;
+    public float energyWaster;
+    float cutOffValue;
+
+    void Start()
+    {
+        GameManager = GameManager.Instance;
+        render = GetComponent<Renderer>();
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cutOffValue = 1/GameManager.TargetEnergy * GameManager.EnergyWastage;
+
+        render.material.SetFloat("_Cutoff", 1-cutOffValue);
+    }
+}
--- GearVRGame/Assets/Scripts/GameManager.cs
+++ GearVRGame/Assets/Scripts/GameManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     [Tooltip("The amount of energy needed to trigger a game-over event.")]
-    public float TargetEnergy = 10000;
+    public float TargetEnergy = 10000f;
 
     [Tooltip("How much energy has been wasted until now.")]
-    public float EnergyWastage = 0;
+    public float EnergyWastage = 0f;
 
     [Tooltip("Modifier that influences how much energy will be wasted.")]
     public float EnergyWasteMultiplier = 1f;
